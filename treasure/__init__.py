4ca7c33ad80018388303d57acbd3a27d16d79500
Joshua Wise <joshua@joshuawise.com>
2020-04-23 00:04:25 -0400

    Support iOS.

--- treasure/__init__.py
+++ treasure/__init__.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import io
+import zlib
+import json
 import uuid
 
 from flask import Flask, jsonify, request, abort
@@ -17,8 +20,6 @@ def submit_event(log):
     # Make up a trace_id, else Honeycomb chokes.
     ev.add_field('trace.trace_id', str(uuid.uuid4()))
     ev.created_at = datetime.utcfromtimestamp(log['time'])
-#    ev.add_field('Timestamp', datetime.utcfromtimestamp(log['time']).isoformat("T") + ".000Z")
-#    ev.add_field('Timestamp', str(log['time'] * 1000))
     ev.add_field('name', log['event'])
     
     # Iterate through fields, providing data for ones in the inclusion list,
@@ -40,6 +41,18 @@ def submit_event(log):
         'device_phone.system_version',
         'device.remote_device.firmware_description.version.firmware.fw_version',
         'device_phone.supports_ble',
+        'carrier_info.mobile_network_code',
+        'device_phone.is_jailbroken',
+        'device.remote_device.firmware_description.version.firmware.fw_version_language_isocode',
+        'carrier_info.mobile_country_code',
+        'app_state.onboarding_complete',
+        'data.button_id',
+        'device_phone.system_build',
+        'carrier_info.carrier_name',
+        'device.remote_device.firmware_description.version.firmware.fw_version_language_version',
+        'device.remote_device.firmware_description.version.firmware.bootloader_version',
+        'carrier_info.iso_country_code',
+        'device.remote_device.firmware_description.version.firmware.fw_version_timestamp',
     ]
     
     # Don't complain about ones on the blacklist.
@@ -52,6 +65,10 @@ def submit_event(log):
         'identity.user',
         'time',
         'event',
+        'pebble_event_uuid',
+        'device_phone.name',
+        'keen.timestamp',
+        'keen.location.coordinates',
     ]
     
     unknown_fields = []
@@ -63,7 +80,7 @@ def submit_event(log):
             continue
         if field not in includelist:
             print(f"CAUTION: unknown field {field} on event {log['event']}")
-            unknown_fields += field
+            unknown_fields += [field]
             continue
         
         ev.add_field(field, data)
@@ -71,16 +88,19 @@ def submit_event(log):
     if len(unknown_fields) > 0:
         ev.add_field("unknown_fields", ','.join(unknown_fields))
     
-    print(ev)
     ev.send()
     
     return { "success": "true" }
 
+@app.route('/ios/v3/event', methods=['POST'])
 @app.route('/android/v3/event', methods=['POST'])
-def android():
+def event_post():
     print(request.headers)
-    req = request.json
-    print(req)
+    if getattr(request, 'content_encoding', None) == 'deflate':
+        rdata = zlib.decompress(request.data)
+    else:
+        rdata = request.data
+    req = json.loads(rdata)
     
     # The only table we support right now is pebble.phone_events.
     resp = {}
